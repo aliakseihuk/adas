c57b55e443d6e5c531cce9ee1139e42008e43de5
aliakseihuk <alexeyguk@gmail.com>
2015-05-15 21:33:41 +0300

    Added arduiono test project

---
+++ adas/Adas.ArduinoTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adas.ArduinoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var arduino = new SerialPort("COM4", 9600))
+            {
+                arduino.Open();
+                var random = new Random();
+                //while (true)
+                //{
+                //    //var message = arduino.ReadLine();
+                //    //Console.WriteLine(message);
+                arduino.Write(new [] {Convert.ToByte(180)}, 0, 1);
+               // }
+                arduino.Close();
+            }
+        }
+    }
+}
